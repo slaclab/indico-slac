ab796aa6f4202409990984deb2484d2ef439eb74
Andy Salnikov <salnikov@slac.stanford.edu>
2019-05-21 17:11:56 -0700

    Use indico-worker for celery

--- indico-worker/docker-entrypoint.sh
+++ indico-worker/docker-entrypoint.sh
@@ -29,8 +29,8 @@ export INDICO_CONFIG=$INDICO_DIR/data/etc/indico.conf
 mkdir -p ${INDICO_DIR}/data/etc ${INDICO_DIR}/data/archive
 mkdir -p ${INDICO_DIR}/scratch/log ${INDICO_DIR}/scratch/tmp ${INDICO_DIR}/scratch/cache
 
-# for all other options we need config files
-if [ "$1" = "run" ]; then
+# for regular options we need config files
+if [ "$1" = "run" -o "$1" = "celery" ]; then
     for f in $CONFIG_FILES; do
         if [ ! -f "$f" ]; then
             cat >&2 <<EOWARN
@@ -43,7 +43,12 @@ EOWARN
             exit 2
         fi
     done
+fi
+
+if [ "$1" = "run" ]; then
     exec /usr/bin/uwsgi --ini /etc/uwsgi.ini
+elif [ "$1" = "celery" ]; then
+    exec indico celery worker -B
 else
     exec "$@"
 fi
